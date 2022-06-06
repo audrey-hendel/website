2a67dbd332b5501962b42930e5f44967bc180b9a
vosaul <v986963@gmail.com>
2022-06-06 19:55:14 +0300

    testimonials page responsive

--- src/pages/testimonials.js
+++ src/pages/testimonials.js
@@ -14,10 +14,17 @@ const TmGrid = styled.div`
   display: grid;
   gap: 70px;
   margin-top: 100px;
+  @media (min-width: 768px){
+    overflow: hidden;
+  }
+  @media (min-width: 1210px){
+    overflow: visible;
+  }
 `
 const TmCard = styled.div`
   display: grid;
   gap: 30px;
+  position: relative;
   @media (min-width: 768px) {
     &:nth-child(odd) {
     grid-template-columns: 1fr 4fr;
@@ -37,32 +44,28 @@ const TmCard = styled.div`
    &:nth-child(odd) .content {
     padding: 65px 60px 65px 200px;
     margin: 0 0 0 -10%;
+    @media (min-width: 768px){
+        padding: 65px 60px 65px 70px;
+      }
+      @media (min-width: 1024px){
+        padding: 65px 60px 65px 120px;
+      }
+      @media (min-width: 1440px){
+        padding: 65px 60px 65px 200px;
+      }
     }
     &:nth-child(even)  .content {
     padding: 65px 200px 65px 60px;
     margin: 0 -10% 0 0;
+    @media (min-width: 768px){
+        padding: 65px 60px 65px 60px;
+      }
+      @media (min-width: 1024px){
+        padding: 65px 200px 65px 60px;
+      }
     }
   }
   
-.gatsby-image-wrapper {
-    justify-self: center;
-    z-index: 5;
-    background: #fff;
-    align-self: center;
-    border-radius: 50%;
-    border: 4px solid #bbb;
-    width: 290px;
-    height: 290px;
-    padding: 30px;
-    img {
-      object-fit: cover;
-      opacity: 1;
-      height: auto;
-      width: 90%;
-      margin: auto;
-      border-radius: 65px;
-    }
-}
 `
 const Content = styled.div`
   background: rgba(221, 161, 192, 0.05);
@@ -70,7 +73,90 @@ const Content = styled.div`
   box-shadow: 0px 4px 4px rgba(51, 51, 51, 0.1);
   border-radius: 30px;
   display: grid;
-  padding: 20px;
+  padding: 157px 20px 20px;
+  margin-top: -125px;
+  @media (min-width: 500px){
+    margin-top: -100px;
+  }
+  @media (min-width: 768px){
+    padding: 20px;
+    margin-top: 0;
+  }
+  .name{
+    font-size: 14px;
+    font-family: 'Sitka';
+    font-style: normal;
+    font-weight: 400;
+    color: rgba(196, 196, 196, 1);
+    @media (min-width: 768px){
+      font-size: 18px;
+    }
+  }
+  .quoTop{
+    img{
+      width: auto;
+      height: auto;
+      transform: rotate(180deg);
+    }
+  }
+  .quotBottom{
+    img{
+      width: auto;
+      height: auto;
+    }
+  }
+`
+const AvatarBox = styled.div`
+position: relative;
+display: grid;
+justify-items: center;
+@media (min-width: 768px){
+  display: block;
+}
+.gatsby-image-wrapper {
+  position: relative;
+  &:nth-child(1){
+    justify-self: center;
+    z-index: 5;
+    background: #fff;
+    align-self: center;
+    border-radius: 50%;
+    border: 3px solid rgba(51, 51, 51, 0.16);
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1); 
+    width: 290px;
+    height: 290px;
+    padding: 30px;
+    position: relative;
+    img {
+      object-fit: cover;
+      opacity: 1;
+      height: auto;
+      width: auto;
+      margin: auto;
+      border-radius: 65px;
+      @media (min-width: 768px){
+        width: 85%;
+      }
+    }
+
+  }
+}
+  .avatar{
+    position: relative;
+  }
+  .decor{
+    position: absolute;
+    width: auto;
+    margin-left: auto;
+    margin-right: auto;
+    overflow: hidden;
+    margin-top: 100px;
+    @media (min-width: 768px){
+      width: 155%;
+      margin-left: -381px;
+      margin-top: 114px;
+    }
+  }
 `
 const Stars = styled.div`
   display: inline-block;
@@ -105,14 +191,26 @@ const Rating = styled.div`
 `
 const Date = styled.p`
   text-align: center;
+  font-size: 14px;
+  font-family: 'Damion';
+  font-style: italic;
+  font-weight: 400;
+  color: rgba(196, 196, 196, 1);
+  @media (min-width: 500px){
+  font-size: 18px;
+}
+
 `
 const Response = styled.p`
 font-family: 'Sitka';
 font-style: normal;
 font-weight: 400;
-font-size: 22px;
+font-size: 16px;
 line-height: 1.3;
 color: rgba(51, 51, 51, 0.8);
+@media (min-width: 500px){
+  font-size: 22px;
+}
 `
 const DecoOuterTop = styled.div`
   width: 95%;
@@ -168,9 +266,7 @@ const DecoInner = styled.div`
   @media (min-width: 1024px){
     padding: 60px 0;}
 `
-const AvatarBox = styled.div`
-    background: url(/images/decor-testimonials.png);
-`
+
 
 const SecondPage = () => (
   <Layout path="/testimonials">
@@ -185,16 +281,19 @@ const SecondPage = () => (
       {pageData.items.map((ct, i) => {
         return (
           <TmCard>
-          <AvatarBox>
-            <GatsbyImage image={GetImageByName(ct.image)} alt="avatar" className="tmAvatar" />
-          </AvatarBox>
+            <AvatarBox>
+              <GatsbyImage image={GetImageByName(ct.image)} alt="avatar" className="tmAvatar" />
+              <GatsbyImage image={GetImageByName('decoration-test.png')} alt='decor' className='decor' />
+            </AvatarBox>
             <Content className="content">
-              <Response>{ct.response}</Response>
-              <p>{ct.name}</p>
+            <GatsbyImage image={GetImageByName('quot.png')} alt='decor' className='quoTop' />
+              <Response >{ct.response}</Response>
+              <p className="name">{ct.name}</p>
               <Rating>
                 <Stars className={'stars-' + ct.stars} />
-              </Rating>AvatarBox
+              </Rating>
               <Date>{ct.date}</Date>
+              <GatsbyImage image={GetImageByName('quot.png')} alt='decor' className='quotBottom' />
             </Content>
           </TmCard>
         )
